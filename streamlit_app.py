41c2bd021a6fa27f184c231a233cc8051b9ae7d8
saria-a-snowflake <114830656+saria-a-snowflake@users.noreply.github.com>
2022-10-04 20:34:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,10 @@ streamlit.dataframe(fruits_to_show)
 # Display the table on the page.
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # display beautiful
